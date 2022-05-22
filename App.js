30d7d16d5f0b7582b1a714b5238cd7489a107527
Do Hoang Duc <59699738+jasondohoang@users.noreply.github.com>
2022-05-22 07:41:17 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import ClothesList from './pages/ClothesList';
+import Contact from './pages/Contact';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
+import Cart from './pages/Cart';
+import Footer from "./components/Footer";
+import ClothesDetail from './pages/ClothesDetail';
+
+
+
+
+
+function App() {
+  return (
+<div>
+   
+
+<Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' index element={<Home/>} /> 
+        <Route path='/home' element={<Home/>} />
+        <Route path='/Clothes-List' element={<ClothesList/>} />
+        <Route path='/contact-us' element={<Contact/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/clothes-detail' element={<ClothesDetail/>}/>
+        
+
+
+      </Routes>
+
+
+      <Footer /> 
+    
+    </Router>
+    
+
+  
+    
+    </div>
+
+
+
+  )
+}
+
+export default App;
+
+
+
